f1c59037ac973407c3a0e3b519368915b52a0916
KISHORE GANIGAPENTA <kganigapenta@corp.paradigmit.com>
2018-07-02 17:12:44 +0530

    cleared unused demos

--- src/demo/app-routing.module.ts
+++ src/demo/app-routing.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 import { BookComponent } from './book/book.component';
-import { CityComponent } from './city/city.component';
-import { RoomComponent } from './room/room.component';
-import { DropdownTreeviewSelectDemoComponent } from './dropdown-treeview-select/dropdown-treeview-select-demo.component';
-import { ProductComponent } from './product/product.component';
+
 
 const routes: Routes = [
     { path: '', redirectTo: 'components', pathMatch: 'full' },
     { path: 'components', component: BookComponent },
-    { path: 'pipe', component: CityComponent },
-    { path: 'performance', component: RoomComponent },
-    { path: 'template', component: DropdownTreeviewSelectDemoComponent },
-    { path: 'advanced', component: ProductComponent },
     { path: '**', component: NotFoundComponent }
 ];
 
--- src/demo/app.module.ts
+++ src/demo/app.module.ts
@@ -6,9 +6,6 @@ import { TreeviewModule } from '../lib';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BookComponent } from './book/book.component';
-import { CityComponent } from './city/city.component';
-import { RoomComponent } from './room/room.component';
-import { ProductComponent } from './product/product.component';
 import { NotFoundComponent } from './not-found.component';
 import { DropdownTreeviewSelectModule } from './dropdown-treeview-select';
 import { I18n } from './i18n';
@@ -26,9 +23,6 @@ import { DisabledOnSelectorDirective } from './disabled-on-selector.directive';
     declarations: [
         NotFoundComponent,
         BookComponent,
-        CityComponent,
-        RoomComponent,
-        ProductComponent,
         AppComponent,
         DisabledOnSelectorDirective
     ],
